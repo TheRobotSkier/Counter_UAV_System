ebf563e13b6fc5c6e4547418b76d8fcbdb1ccde5
MortenSGitVaerk <mso22@student.aau.dk>
2025-11-10 11:12:21 +0100

    Add UAV_Tracker_Particle_Filter.py with input/output specifications

---
+++ UAV_Tracker_Particle_Filter.py
@@ -0,0 +1,23 @@
+# input will be on the form of:
+# From point pillars:
+# 1 from center point of bounding box (x,y)
+# 2 width of bounding box
+# 3 height of bounding box
+# 4 frame number
+# 5 confidence score
+# Aquistic DOA vector:
+# 6 azimuth angle
+# 7 elevation angle
+# 8 confidence score
+# 9 frame number
+
+# Output will be on the form of:
+# 1 from center point of bounding box (x,y)
+# 2 width of bounding box
+# 3 height of bounding box
+# 4 frame number
+# 5 confidence score
+# 6 azimuth angle
+# 7 elevation angle
+# 8 confidence score
+
