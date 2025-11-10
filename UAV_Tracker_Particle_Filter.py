f03d64c1cf255e664863ae87b3c52ee3ccccb4c7
MortenSGitVaerk <mso22@student.aau.dk>
2025-11-10 12:54:30 +0100

    Refactor input/output specifications in UAV_Tracker_Particle_Filter.py for clarity and completeness

--- UAV_Tracker_Particle_Filter.py
+++ UAV_Tracker_Particle_Filter.py
@@ -1,23 +1,44 @@
+import numpy as np
+import numpy.ma as ma
+
 # input will be on the form of:
 # From point pillars:
-# 1 from center point of bounding box (x,y)
-# 2 width of bounding box
-# 3 height of bounding box
-# 4 frame number
-# 5 confidence score
-# Aquistic DOA vector:
-# 6 azimuth angle
-# 7 elevation angle
-# 8 confidence score
-# 9 frame number
+# 1 Position of UAV bounding box (x,y,z) in world coordinates (ask about format) 
+# 2 Orientation of UAV bounding box in world coordinates (ask about format)
+# 3 Dimentions of the bounding box (length, width, height)
+# 4 Confidence score
+# 5 timestamp
 
-# Output will be on the form of:
-# 1 from center point of bounding box (x,y)
-# 2 width of bounding box
-# 3 height of bounding box
-# 4 frame number
-# 5 confidence score
-# 6 azimuth angle
-# 7 elevation angle
-# 8 confidence score
+# Aquistic DOA vector:
+# 6 azimuth angle 
+# 7 elevation angle 
+# 8 confidence score 
+# 9 timestamp
+# maybe multiple ones of these per frame
+# no previos weight to account for inacurasy
+
+#additionsal information:
+# timestamp
+# camera frame position and orientation in world coordinates
+
+# Output will be used to controll the pan tilt system to point at the UAV:
+# 1 Position of UAV bounding box (x,y,z) in world coordinates (ask about format) 
+# 2 Orientation of UAV bounding box in world coordinates (ask about format)
+# 3 Timestamp
+# maybe add confidence score of the estimated position
+# maybe add velocity vector of the UAV
+# maybe add acceleration vector of the UAV
+# maybe add number of particles used in the estimation
+# maybe add resampling information
+AqData = np.array([37,50])
+PointPillarData = np.array([])
+UAV_Estimation = np.array([])
+
+def AquisticDOAData(AqData_Input): 
+    DOAData = AqData_Input
+    return DOAData
+
+Print_DOA_Text = AquisticDOAData(AqData)
+print("DOA Data: ", Print_DOA_Text)
+
 
