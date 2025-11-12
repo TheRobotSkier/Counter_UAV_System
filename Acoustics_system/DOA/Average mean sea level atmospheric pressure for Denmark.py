7f5c8e09c35388df2f4efcf69103f6032b3ee5c3
TheRobotSkier <emil.hausberger.htxq16@gmail.com>
2025-11-12 10:51:18 +0100

    Atmospheric pressure and temperature sensor code for Arduino, via serial port, send to Python for calculation of the speed of sound in air.   Simulation of time difference for a regular tetrahedron-shaped microphone array in Python.
    Atmospheric pressure and temperature sensor code for Arduino, via serial port, send to Python for calculation of the speed of sound in air.
    
    Simulation of time difference for a regular tetrahedron-shaped microphone array in Python.

---
+++ Acoustics_system/DOA/Average mean sea level atmospheric pressure for Denmark.py
@@ -0,0 +1,9 @@
+values = [
+    1012.2, 1013.7, 1013.4, 1012.2,
+    1013.5, 1014.1, 1014.4, 1013.0,
+    1012.7, 1013.6, 1013.2, 1012.6,
+    1013.5, 1014.1, 1013.6, 1013.5,
+    1013.9, 1014.6, 1014.0, 1014.0,
+]
+mean = sum(values) / len(values)
+print(f"Average mean sea level atmospheric pressure for Denmark:\n {mean} hpa")  # 1013.49 hpa
