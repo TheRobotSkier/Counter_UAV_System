eb7865f1cadaf122f92d2ba4a423f710d29d2ea5
Gustav Bay <gustavbay@live.dk~>
2025-12-01 10:30:26 +0100

    Updated run_model script

--- Drone-PointPillars/Drone_PointPillars/run_model.py
+++ Drone-PointPillars/Drone_PointPillars/run_model.py
@@ -3,25 +3,6 @@ import numpy as np
 from drone_pointpillars.model import PointPillars
 from drone_pointpillars.utils import keep_bbox_from_lidar_range
 
-point_cloud = {}
-
-# Parmeters
-pcd_limit_range = np.array([0, -40, -40, 70, 40, 40], dtype=np.float32)
-
-# Init model (Choose small or large model)
-if True:
-    # Ssmall model 
-    trained_path = "/path/to/the/Small.pth"
-    model = PointPillars(nclasses=1).cuda()
-else:
-    # Large model
-    trained_path = "/path/to/the/Large.pth"
-    model = PointPillars(nclasses=1, max_num_points=100, Backbone_layer_strides=[1,2,2],upsample_strides=[1,2,4]).cuda()
-    
-# Load weights
-model.load_state_dict(torch.load(trained_path))
-model.eval()
-
 def point_range_filter(pts, point_range=[0, -40, -40, 70, 40, 40]):
     '''
     data_dict: dict(pts, gt_bboxes_3d, gt_labels, gt_names, difficulty)
@@ -37,21 +18,58 @@ def point_range_filter(pts, point_range=[0, -40, -40, 70, 40, 40]):
     pts = pts[keep_mask]
     return pts 
 
-with torch.no_grad():
-    while True:
-        # Remove points out of range 
-        pc = point_range_filter(pc, pcd_limit_range)
+def load_model_checkpoint(trained_path,model):
+    ckpt = torch.load(trained_path)
 
-        # Run the model
-        result = model(batched_pts=pc, mode='test')
+    # Load weights
+    if isinstance(ckpt, dict):
+            if 'model' in ckpt:
+                state = ckpt['model']
+            elif 'state_dict' in ckpt:
+                state = ckpt['state_dict']
+            elif 'model_state_dict' in ckpt:
+                state = ckpt['model_state_dict']
+            else:
+                # assume whole dict is a state_dict
+                state = ckpt
+    else:
+        # old-style: ckpt *is* the state_dict
+        state = ckpt
 
-        # Remove predictions out of range
-        result_filter = keep_bbox_from_lidar_range(result, pcd_limit_range)
+    model.load_state_dict(state)
 
-        # Split up results
-        lidar_bboxes = result_filter['lidar_bboxes']
-        labels, scores = result_filter['labels'], result_filter['scores']
+def main():
+    point_cloud = {}  # Replace with actual point cloud data loading
 
+    # Parameters
+    pcd_limit_range = np.array([0, -40, -40, 70, 40, 40], dtype=np.float32)
 
+    # Init model (Choose small or large model)
+    if True:
+        # Ssmall model 
+        trained_path = "/path/to/the/Small.pth"
+        model = PointPillars(nclasses=1).cuda()
+    else:
+        # Large model
+        trained_path = "/path/to/the/Large.pth"
+        model = PointPillars(nclasses=1, max_num_points=100, Backbone_layer_strides=[1,2,2],upsample_strides=[1,2,4]).cuda()
+        
+    load_model_checkpoint(trained_path, model)
+    model.eval()
 
+    with torch.no_grad():
+        while True:
+            # Remove points from point cloud which is out of range 
+            pc = point_range_filter(pc, pcd_limit_range)
 
+            # Run the model
+            result = model(batched_pts=pc, mode='test')
+
+            # Remove predictions which is out of range
+            result_filter = keep_bbox_from_lidar_range(result, pcd_limit_range)
+
+            # Split up results
+            lidar_bboxes = result_filter['lidar_bboxes']
+            labels, scores = result_filter['labels'], result_filter['scores']
+
+main()
