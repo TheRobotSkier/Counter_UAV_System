a84071edb5856239f34f89e340ac02b50bd1cab2
Dripmeister64 <mso22@student.aau.dk>
2025-12-05 08:53:48 +0100

    Helllo i make transform

---
+++ Transform_Functions.py
@@ -0,0 +1,6 @@
+#The first transform is used to locate the center of the Aqoustic array foot
+GNNSRTK = [0, 0, 0]
+GNNSRTK_To_Aquostic_Array_Foot = [-190.5, 0, 0]
+Aquostic_Array_Foot = [, 0, 0]
+
+
